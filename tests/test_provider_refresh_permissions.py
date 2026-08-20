3cbfec62560118b7633cfb0d068a3afeeeea902f
Bhuvaneshwaran Murugan <bhuvaneshwaranmurugan21@gmail.com>
2026-08-20 13:52:18 +0530

    Reconcile Step Functions validation permission

--- tests/test_provider_refresh_permissions.py
+++ tests/test_provider_refresh_permissions.py
@@ -15,7 +15,10 @@ def test_provider_refresh_permissions_are_bounded() -> None:
 
     bucket_actions = set(statements["StateAndLabBuckets"]["Action"])
     lambda_actions = set(statements["AtlasLambda"]["Action"])
+    states_validation = statements["GlobalStepFunctionsValidation"]
 
     assert "s3:GetObjectTagging" in bucket_actions
     assert "lambda:ListVersionsByFunction" in lambda_actions
+    assert states_validation["Action"] == "states:ValidateStateMachineDefinition"
+    assert states_validation["Resource"] == "*"
     assert all("*" not in action for action in bucket_actions | lambda_actions)
